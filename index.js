19ed1d10513ff38162069b0abb0f5c225ef691f2
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-11 22:22:32 -0700

    commit tree 2303b92de2f27c4a5112f97d9d2c0ea0374612fc

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+//The function returns a string like "This Saturday, I want to ....!"
+//If nothing is passed in, default to "roller-skate".
+function saturdayFun(activity="roller-skate") {
+    console.log(`This Saturday, I want to ${activity}!`);
+    return (`This Saturday, I want to ${activity}!`);
+}
+saturdayFun() ;
+
+//The function returns a string "This Monday, I want to ... ." 
+//If nothing is passed in, default to "go to the office".
+const mondayWork = function(activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}`);
+     return `This Monday, I will ${activity}.`;
+}
+
+mondayWork();
+
+//Function wrapAdjective return a inner function.
+//This "inner" function takes a single parameter that default to "special"(it's an adjective).
+//return a String of the form "You are ..." where ... is the adjective this function received wrapped in visual flair.
+//It should take as parameter 'String' that will be used to create visual flair
+
+function wrapAdjective(string = "*") {
+
+    return function (x = 'special') {
+            return (`You are ${string}${x}${string}!`);
+        }
+}
+
+
+wrapAdjective("<")("a dedicated programmer");
